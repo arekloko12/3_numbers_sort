b4a13c790fb56c200bcfa60e36cf72bb67179b51
arekloko12 <arkadiuszkornecki12@gmail.com>
2024-01-13 20:20:57 +0100

    first commit

---
+++ sort_3_numbers.py
@@ -0,0 +1,32 @@
+a = int(input("Enter the 1-st number "))
+b = int(input("Enter the 2-nd number "))
+c = int(input("Enter the 3-td number "))
+
+table_of_numbers = []
+
+if a >= b:
+    if a >= c:
+        table_of_numbers.append(str(a)+"(a)")
+        if b >= c:
+            table_of_numbers.append(str(b)+"(b)")
+            table_of_numbers.append(str(c)+"(c)")
+        else:
+            table_of_numbers.append(str(c)+"(c)")
+            table_of_numbers.append(str(b)+"(b)")
+    else:
+        table_of_numbers.append(str(c)+"(c)")
+        table_of_numbers.append(str(a)+"(a)")
+        table_of_numbers.append(str(b)+"(b)")
+else:
+    if a >= c:
+        table_of_numbers.append(str(b)+"(b)")
+        table_of_numbers.append(str(a)+"(a)")
+        table_of_numbers.append(str(c)+"(c)")
+    else:
+        if b >= c:
+            table_of_numbers.append(str(b)+"(b)")
+            table_of_numbers.append(str(c)+"(c)")
+            table_of_numbers.append(str(a)+"(a)")
+
+for i in range(3):
+    print(table_of_numbers[i], end=", ")
